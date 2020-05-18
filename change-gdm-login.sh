bea23e798481cac8861c11bfa253b02a498491e5
Kurt Krause <jurgen.krause@gmail.com>
2020-05-17 21:58:27 -0300

    Refactored post-install.sh, added gdm-login changer, added abnt keyboard switch because gnome was being weird about it

---
+++ change-gdm-login.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Change GDM login screen
+sudo update-alternatives --config gdm3-theme.gresource
--- post-install.sh
+++ post-install.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
 ## New folders to be used for .deb downloads and github repository plus any folder you need because you want to
-mkdir "/home/kurt/Downloads/postinstall"
-mkdir "/home/kurt/Repo"
-mkdir "/home/kurt/Appimgs"
+mkdir "/home/$USER/Repo"
 
-URL_RUNELITE="https://github.com/runelite/launcher/releases/download/2.1.0/RuneLite.AppImage"
-URL_UNITY="https://public-cdn.cloud.unity3d.com/hub/prod/UnityHub.AppImage"
-
-DOWNLOAD_DIR="/home/kurt/Downloads/postinstall"
-REPO_DIR="/home/kurt/Repo"
-APPIMG_DIR="/home/kurt/Appimgs"
+DOWNLOAD_DIR="/home/$USER/Downloads"
+REPO_DIR="/home/$USER/Repo"
+APPIMG_DIR="/home/$USER/Appimgs"
 
 ## Remove apt locks if any
 sudo rm /var/lib/dpkg/lock-frontend
@@ -19,43 +14,43 @@ sudo rm /var/cache/apt/archives/lock
 ## Repository update
 sudo apt update -y
 
-## EXEC
-
-## Update repository after 3rd-party repos have been added
-sudo apt update -y
-
 # Instalall apt`s
-sudo apt install git tilix guake tlp tlp-rdw snapd htop neofetch virtualbox tilix inkscape vlc krita gimp speedtest-cli mongodb zsh fonts-powerline curl gnome-tweaks apt-transport-https gnome-session -y
+sudo apt install git guake steam lutris vlc snapd flatpak htop neofetch virtualbox tilix speedtest-cli mongodb zsh fonts-powerline curl wget gnome-tweaks apt-transport-https gnome-session libgtk-3-dev git meson sassc inkscape optipng ruby -y
 
 ## Install flatpaks
-#flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub net.runelite.RuneLite -y
+flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub com.unity.UnityHub -y
+flatpak install flathub org.gimp.GIMP -y
+flatpak install flathub org.kde.krita -y
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub com.axosoft.GitKraken -y
+flatpak install flathub com.jagex.RuneScape -y
+flatpak install flathub org.blender.Blender -y
+flatpak install flathub com.github.debauchee.barrier -y
+flatpak install flathub com.anydesk.Anydesk -y
+flatpak install flathub org.audacityteam.Audacity -y
+flatpak install flathub org.gnome.meld -y
+flatpak install flathub com.basemark.BasemarkGPU -y
 
 ## Install snaps
-sudo snap install spotify
 sudo snap install code --classic
 sudo snap install postman
+sudo snap install atom --classic
 sudo snap install android-studio --classic
-sudo snap install gitkraken
-sudo snap install node --classic
+sudo snap install node --channel=14/stable --classic
 sudo snap install dotnet-sdk --classic
 sudo snap install snapcraft --classic
-sudo snap install discord
 sudo snap install rider --classic
 sudo snap install datagrip --classic
 sudo snap install webstorm --classic
 sudo snap install pycharm-professional --classic
 #sudo snap install pycharm-community --classic
 sudo snap install aws-cli --classic
-sudo snap install runelite
 sudo snap install dwarf-fortress
 sudo snap install openosrs 
-sudo snap install obs-studio
-
-
-## Manual instalations
-
-# Unity
-wget -c "$URL_UNITY" -P "$APPIMG_DIR"
 
 ## Post-install update and cleanup
 sudo apt update && sudo apt dist-upgrade -y
